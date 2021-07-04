904258a70abe1460830bb31cbafd6d33b392b289
wenwood <wenwood@live.com>
2021-07-04 13:27:28 -0400

    update server to use my http parser

--- Nano_server.c
+++ Nano_server.c
@@ -12,6 +12,7 @@
 #include <sys/stat.h>
 #include <libgen.h>
 #include <sys/wait.h>
+#include "utils/http_parser/http_parser.h"
 
 #define PORT 8080
 #define BUF 1024
@@ -43,38 +44,6 @@ void logging(const char *format_str, ...)
     va_end(arglist);
 }
 
-/**
- * readline a line from socket, and put it in buf.
- * @RETURN
- * on success, return number of character stored in buf
- * on error, -1 or -2 is returned, -1 indicate recv system call error, -2 indicate buf is too small. 
-*/
-
-ssize_t readline(int client_sock, char *buf, size_t buf_len) // need update
-{
-    char local_buf[BUF];
-    // find where the line end
-    ssize_t recv_ret = recv(client_sock, local_buf, sizeof(local_buf) - 1, MSG_PEEK);
-    if (recv_ret == -1)
-    {
-        return -1;
-    }
-    local_buf[recv_ret] = '\0';
-    int should_read = strstr(local_buf, "\r\n") - local_buf + 2;
-    if (should_read == NULL)
-    {
-        return -2;
-    }
-    // read until the line end
-    // should_read won't be bigger than buf_len, but what if it is bigger than buf_len?
-    recv_ret = recv(client_sock, buf, should_read, 0);
-    if (recv_ret == -1)
-    {
-        return -1;
-    }
-    buf[recv_ret] = '\0';
-    return recv_ret;
-}
 
 /**
  * show the error page, tell client the server got a bad request
@@ -83,7 +52,7 @@ ssize_t readline(int client_sock, char *buf, size_t buf_len) // need update
 void bad_request(int client_sock)
 {
     char buf[1024];
-    sprintf(buf, "HTTP/1.1 400 Bad Request\r\nContent-Type: text/html\r\n\r\n\<p>Please check your request.</p>");
+    sprintf(buf, "HTTP/1.1 400 Bad Request\r\nContent-Type: text/html\r\n\r\n<p>Please check your request.</p>");
     send(client_sock, buf, strlen(buf), 0);
 }
 
@@ -112,7 +81,6 @@ void not_found(int client_sock, char *url)
 {
     char buf[1024];
     sprintf(buf, "HTTP/1.0 404 NOT FOUND\r\nContent-Type: text/html\r\n\r\n<html><head>404</head><body><p>the page you request : %s is not found.</p></body></html>\r\n",url);
-    puts(buf);
     send(client_sock, buf, strlen(buf), 0);
     puts("not_found");
 }
@@ -123,10 +91,10 @@ void not_found(int client_sock, char *url)
 */
 
 
-void unimplemented(int client_sock, char *method)
+void unimplemented(int client_sock)
 {
     char buf[1024];
-    sprintf(buf, "HTTP/1.1 400 Bad Request\r\nContent-Type: text/html\r\n\r\n<p>request method %s is not implemented yet.</p>\r\n", method);
+    sprintf(buf, "HTTP/1.1 400 Bad Request\r\nContent-Type: text/html\r\n\r\n<p>request method is not implemented yet.</p>\r\n");
     send(client_sock, buf, strlen(buf), 0);
 }
 
@@ -138,8 +106,6 @@ void return_file(int client_sock, char *url)
 {
     char buf[1024];
     bzero(buf, 0);
-    while (strcmp(buf, "\r\n") != 0 && (readline(client_sock, buf, sizeof(buf) - 1) > 0))
-        ;
     FILE *fp = fopen(url, "r");
     if (fp == NULL)
     {
@@ -147,7 +113,6 @@ void return_file(int client_sock, char *url)
     }
     sprintf(buf, "HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n");
     send(client_sock, buf, strlen(buf), 0);
-    size_t n = 0;
     while (fgets(buf, sizeof(buf), fp) != NULL)
     {
         send(client_sock, buf, strlen(buf), 0);
@@ -159,19 +124,20 @@ void return_file(int client_sock, char *url)
  * unfinished
 */
 
-void exec_cgi(int client_sock, char *url)
+void exec_cgi(int client_sock, Http_request_t *req)
 {
     char line_buf[1024];
     size_t content_length = 0;
-    do
+    Http_header *hp = req->header;
+    while(hp != NULL)
     {
-        readline(client_sock, line_buf, sizeof(line_buf));
-        if (strstr(line_buf, "Content-Length:") != NULL)
+        if (strstr(hp->key, "Content-Type") != NULL)
         {
-            content_length = atoi(strlen("Content-Length:") + line_buf);
+            content_length = atoi(hp->value);
+            break;
         }
-
-    } while (strcmp(line_buf, "\r\n") != 0);
+        hp = hp->next;
+    }
 
     int cgi_input[2];
     int cgi_output[2];
@@ -188,10 +154,9 @@ void exec_cgi(int client_sock, char *url)
         close(cgi_output[0]);
         dup2(cgi_input[0], STDIN_FILENO);
         dup2(cgi_output[1], STDOUT_FILENO);
-        sprintf(line_buf, "Content-Length=%d", content_length);
+        sprintf(line_buf, "Content-Length=%zu", content_length);
         putenv(line_buf);
-
-        execl(url, basename(url), (char *)NULL);
+        execl(req->url, basename(req->url), (char *)NULL);
     }
 
     //parent
@@ -223,42 +188,31 @@ void *accept_request(void *arg)
     getpeername(client_sock, (struct sockaddr *)&client_addr, &client_addr_len);
     char client_ip[100];
     inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, sizeof(client_ip));
-    puts(client_ip);
+    Http_request_t req;
+    http_parser_init(req);
+    char buf[4096];
+    ssize_t recvived = recv(client_sock, buf, sizeof(buf), 0);
+    buf[recvived] = '\0';
+    http_parser(buf, strlen(buf), &req);
+    modify_url(req.url);
+    
 
-    char line_buf[BUF];
-    if (readline(client_sock, line_buf, sizeof(line_buf)) == -1)
+    if (req.method != GET && req.method != POST)
     {
-        exit_on_error("CLIENT(%s) : %s", client_ip, "readline error!");
-    }
-    char http_request_method[10];
-    char http_request_url[1024];
-    char http_request_ver[10];
-    if (sscanf(line_buf, "%s %s %s", http_request_method, http_request_url, http_request_ver) != 3)
-    {
-        bad_request(client_sock);
-        pthread_exit(NULL);
-    }
-    modify_url(http_request_url);
-
-    if (strcmp("GET", http_request_method) != 0 && strcmp("POST", http_request_method) != 0)
-    {
-        unimplemented(client_sock, http_request_method);
+        unimplemented(client_sock);
         pthread_exit(NULL);
     }
 
     struct stat request_file;
-    if (stat(http_request_url, &request_file) == -1)
+    if (stat(req.url, &request_file) == -1)
     {
         if (errno == ENOENT)
         {
-            char buf[1024];
             bzero(buf, 0);
-            ssize_t n;
-            int i = 0;
-            while (strcmp(buf, "\r\n") != 0 && (readline(client_sock, buf, sizeof(buf) - 1) > 0))
+            while (strcmp(buf, "\r\n") != 0 && (read(client_sock, buf, sizeof(buf)) > 0))
                 ;
 
-            not_found(client_sock, http_request_url);
+            not_found(client_sock, req.url);
             close(client_sock);
             pthread_exit(NULL);
         }
@@ -266,11 +220,11 @@ void *accept_request(void *arg)
 
     if ((request_file.st_mode & S_IXUSR) || (request_file.st_mode & S_IXGRP) || (request_file.st_mode & S_IXOTH))
     {
-        exec_cgi(client_sock, http_request_url);
+        exec_cgi(client_sock, &req);
     }
     else
     {
-        return_file(client_sock, http_request_url);
+        return_file(client_sock, req.url);
     }
     close(client_sock);
     pthread_exit(NULL);
